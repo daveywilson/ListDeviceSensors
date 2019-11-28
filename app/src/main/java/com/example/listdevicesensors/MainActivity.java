4a3a146a4579ce715b3e301d7dfa54860d021168
Daveywilson <wilson-d31@ulster.ac.uk>
2019-11-28 14:57:52 +0000

    List Device Sensors

--- app/src/main/java/com/example/listdevicesensors/MainActivity.java
+++ app/src/main/java/com/example/listdevicesensors/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.listdevicesensors;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView)findViewById(R.id.listViewSensors);
+        List sensorList = new ArrayList<String>();
+        List<Sensor> sensors = ((SensorManager)getSystemService(Context.SENSOR_SERVICE)).getSensorList(Sensor.TYPE_ALL);
+
+       for(Sensor sensor : sensors){
+           sensorList.add(sensor.getName());
+       }
+        ListAdapter sensorAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, sensorList);
+       listView.setAdapter(sensorAdapter);
     }
 }
